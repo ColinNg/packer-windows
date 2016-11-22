381bc73321e98968301969d1cb4f66d3024eec4a
Colin Ng <colin@ngland.net>
2016-11-22 01:15:04 -0800

    Hoping this fixes:
    ==> virtualbox-iso: Provisioning with shell script: ./scripts/dev-tools.ps1
        virtualbox-iso: At C:\Windows\Temp\script.ps1:1 char:47
        virtualbox-iso: + iex ((new-object net.webclient).DownloadString
        virtualbox-iso: +                                               ~
        virtualbox-iso: Missing closing ')' in expression.
        virtualbox-iso: At C:\Windows\Temp\script.ps1:2 char:42
        virtualbox-iso: +    ('https://chocolatey.org/install.ps1'))
        virtualbox-iso: +                                          ~
        virtualbox-iso: Unexpected token ')' in expression or statement.
        virtualbox-iso: + CategoryInfo          : ParserError: (:) [], ParseException
        virtualbox-iso: + FullyQualifiedErrorId : MissingEndParenthesisInExpression

--- scripts/dev-tools.ps1
+++ scripts/dev-tools.ps1
@@ -1,5 +1,4 @@
-iex ((new-object net.webclient).DownloadString
-   ('https://chocolatey.org/install.ps1'))
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install VisualStudio2015Community
 choco install ReSharper
 choco install LINQPad
