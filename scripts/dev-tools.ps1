ff5caa57cc577bfa13f06be7d301088cec351830
Colin Ng <colin@ngland.net>
2016-11-23 20:22:16 -0800

    Automatically agree to the licenses.

--- scripts/dev-tools.ps1
+++ scripts/dev-tools.ps1
@@ -1,6 +1,8 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install VisualStudio2015Community
-choco install ReSharper
-choco install LINQPad
-choco install 7Zip
-choco install SourceTree
+choco install VisualStudio2015Community -y
+choco install ReSharper -y
+choco install LINQPad -y
+choco install 7zip -y
+choco install SourceTree -y
+choco install KDiff3 -y
+choco install SublimeText3 -y
