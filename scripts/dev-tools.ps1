743130082ea79bc3688c36f53fdd37f34c8bcab2
Colin Ng <colin@ngland.net>
2016-11-24 23:50:17 -0800

    Simplify the configuration. Remove the nic2 hack where I tried to fix Chocolatey when in fact the bug was with VirtualBox-5.1.8-111374-OSX: https://www.virtualbox.org/ticket/16084 https://github.com/chocolatey/choco/issues/1029

--- scripts/dev-tools.ps1
+++ scripts/dev-tools.ps1
@@ -1,8 +1,9 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install VisualStudio2015Community -y
-choco install ReSharper -y
-choco install LINQPad -y
-choco install 7zip -y
-choco install SourceTree -y
-choco install KDiff3 -y
-choco install SublimeText3 -y
+choco feature enable -n allowGlobalConfirmation
+choco install VisualStudio2015Community
+choco install ReSharper
+choco install LINQPad
+choco install 7zip
+choco install SourceTree
+choco install KDiff3
+choco install SublimeText3
