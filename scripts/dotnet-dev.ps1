f92d3aa6876590c4e8179634cc329074354f826a
Colin Ng <colin@ngland.net>
2016-11-26 12:40:25 -0800

    windows_2016_dotnet-dev.json is a copy of windows_2016.json with these modifications: 1. Installs my favorite dev tools. 2. A workaround for “Error removing floppy controller”: 	virtualbox-iso: Removing floppy drive... 	==> virtualbox-iso: Error removing floppy controller: VBoxManage error: VBoxManage: error: The machine 'packer-virtualbox-	iso-1479795526' is already locked for a session (or being unlocked) 	==> virtualbox-iso: VBoxManage: error: Details: code VBOX_E_INVALID_OBJECT_STATE (0x80bb0007), component MachineWrap, 	interface IMachine, callee nsISupports 	==> virtualbox-iso: VBoxManage: error: Context: "LockMachine(a->session, LockType_Write)" at line 1038 of file 	VBoxManageStorageController.cpp 	==> virtualbox-iso: Unregistering and deleting virtual machine...
    	References:
    	https://github.com/GoSecure/malboxes/issues/10
    	https://github.com/mitchellh/packer/issues/2401
    	https://github.com/mitchellh/packer/pull/3952#issuecomment-251197612
    	https://www.virtualbox.org/ticket/16063

---
+++ scripts/dotnet-dev.ps1
@@ -0,0 +1,8 @@
+choco install VisualStudio2015Community
+choco install MSSQLServerManagementStudio2014Express
+choco install ReSharper
+choco install LINQPad
+choco install 7Zip
+choco install SourceTree
+choco install KDiff3
+choco install SublimeText3
