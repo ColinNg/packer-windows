26355da3ec944c13d0d8ef25fcdb5704b26b6c2b
Colin Ng <colin@ngland.net>
2016-12-01 21:25:18 -0800

    Added script to disable shutdown tracker in Windows for my dotnet-dev box. Also renamed the output box file of the windows_2016_dotnet-dev.json profile.

---
+++ scripts/disable-shutdown-tracker.bat
@@ -0,0 +1,2 @@
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability" /f /v ShutdownReasonOn /t REG_DWORD /d 0
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability" /f /v ShutdownReasonUI /t REG_DWORD /d 0
