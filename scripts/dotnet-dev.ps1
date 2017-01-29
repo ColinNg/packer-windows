0ce1282cf4555159ac33e3a0c8c219436f24ba27
Colin Ng <colin@ngland.net>
2017-01-28 22:43:01 -0800

    Adding git to the install list.

--- scripts/dotnet-dev.ps1
+++ scripts/dotnet-dev.ps1
@@ -2,6 +2,7 @@ choco install VisualStudio2015Community --execution-timeout=7200
 choco install ReSharper
 choco install LINQPad
 choco install 7Zip
+choco install git
 choco install SourceTree
 choco install KDiff3
 choco install SublimeText3
