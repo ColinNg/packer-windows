353de3dd5ca6a823425eb51c5b9011c45e24e6e5
Colin Ng <colinng@YVRGrid01.vonigo.com>
2017-06-30 15:55:11 -0700

    Workaround for Visual Studio 2015 requesting a restart.
    packer returns this transcript:
    ...
        virtualbox-iso: Packages requiring reboot:
        virtualbox-iso: - visualstudio2015community (exit code 3010)
        virtualbox-iso:
        virtualbox-iso: The recent package changes indicate a reboot is necessary.
        virtualbox-iso: Please reboot at your earliest convenience.
    ...
    Build 'virtualbox-iso' errored: Script exited with non-zero exit status: 1. Allowed exit codes are: [0]
    
    The workaround is to ask chocolatey to return 0 by using the --ignore-package-exit-codes option.
    https://github.com/chef/chef/issues/5030

---
+++ scripts/dotnet-dev-1.ps1
@@ -0,0 +1,1 @@
+powershell choco install VisualStudio2015Community --execution-timeout=7200 --ignore-package-exit-codes
---
+++ scripts/dotnet-dev-2.ps1
@@ -0,0 +1,7 @@
+choco install ReSharper
+choco install LINQPad
+choco install 7Zip
+choco install git
+choco install SourceTree
+choco install KDiff3
+choco install SublimeText3
