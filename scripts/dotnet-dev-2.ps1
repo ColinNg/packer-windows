4ac8b2dc27f20d58674e07284e9867fee83eac0b
Colin Ng <cng@vonigo.com>
2017-07-05 16:13:36 -0700

    Added frequently used packages for Development environments. This will pay off if we do linked clones.

--- scripts/dotnet-dev-2.ps1
+++ scripts/dotnet-dev-2.ps1
@@ -1,7 +1,21 @@
-choco install ReSharper
 choco install LINQPad
 choco install 7Zip
 choco install git
 choco install SourceTree
 choco install KDiff3
 choco install SublimeText3
+
+# General Usage
+cinst GoogleChrome
+
+# UI Testing
+cinst Firefox
+cinst selenium-gecko-driver
+
+# CI
+cinst JRE8
+
+# Developer/QA
+cinst Putty
+cinst sql-server-management-studio
+cinst AgentRansack
--- scripts/dotnet-dev.ps1
+++
@@ -1,8 +0,0 @@
-choco install VisualStudio2015Community --execution-timeout=7200
-choco install ReSharper
-choco install LINQPad
-choco install 7Zip
-choco install git
-choco install SourceTree
-choco install KDiff3
-choco install SublimeText3
