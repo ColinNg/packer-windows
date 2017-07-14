7e444838e4c185d842f34242a06dd418387ccb63
Vonibot <vonibot@DebianVMCreator.vonigo.com>
2017-07-14 09:08:38 -0700

    Install the correct (x86) version of Firefox, and downgrade geckodriver to 0.17.0 to work around a bug. Also add tools commonly used in a Dev environment that are unlikely to interfere with testing.

--- scripts/dotnet-dev-2.ps1
+++ scripts/dotnet-dev-2.ps1
@@ -9,13 +9,20 @@ choco install SublimeText3
 cinst GoogleChrome
 
 # UI Testing
-cinst Firefox
-cinst selenium-gecko-driver
+cinst Firefox --forcex86
+# geckodriver 0.18.0 has a bug with Maximize Window
+# https://github.com/mozilla/geckodriver/issues/820
+# that might be fixed with Firefox 55 which will be out 2017-08-08
+cinst selenium-gecko-driver --version 0.17.0
+cinst nunit-console.portable
 
 # CI
 cinst JRE8
 
 # Developer/QA
+cinst git
 cinst Putty
 cinst sql-server-management-studio
 cinst AgentRansack
+cinst sublimetext3
+cinst kdiff3
