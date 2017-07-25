8edb8b8706ccac60f1b77877dd5edcb0bb297d7b
Colin Ng <cng@vonigo.com>
2017-07-25 10:23:23 -0700

    Trying to stop choco from returning errors during the build process.

--- scripts/dotnet-dev-2.ps1
+++ scripts/dotnet-dev-2.ps1
@@ -3,28 +3,28 @@
 #    virtualbox-iso: from 'https://www.linqpad.net/GetFile.aspx?LINQPad5Setup.exe'
 #Build 'virtualbox-iso' errored: Script exited with non-zero exit status: 16001.
 #Allowed exit codes are: [0]
-# choco install LINQPad
+powershell cinst LINQPad --ignore-package-exit-codes
 
 # General Usage
-cinst GoogleChrome
-cinst 7Zip
+powershell cinst GoogleChrome --ignore-package-exit-codes
+powershell cinst 7Zip --ignore-package-exit-codes
 
 # UI Testing
-cinst Firefox --forcex86
+powershell cinst Firefox --forcex86 --ignore-package-exit-codes
 # geckodriver 0.18.0 has a bug with Maximize Window
 # https://github.com/mozilla/geckodriver/issues/820
 # that might be fixed with Firefox 55 which will be out 2017-08-08
-cinst selenium-gecko-driver --version 0.17.0
-cinst nunit-console.portable
+powershell cinst selenium-gecko-driver --version 0.17.0 --ignore-package-exit-codes
+powershell cinst nunit-console.portable --ignore-package-exit-codes
 
 # CI
-cinst JRE8
+powershell cinst JRE8 --ignore-package-exit-codes
 
 # Developer/QA
-cinst git
-cinst SourceTree
-cinst Putty
-cinst sql-server-management-studio
-cinst AgentRansack
-cinst sublimetext3
-cinst kdiff3
+powershell cinst git --ignore-package-exit-codes
+powershell cinst SourceTree --ignore-package-exit-codes
+powershell cinst Putty --ignore-package-exit-codes
+powershell cinst sql-server-management-studio --ignore-package-exit-codes
+powershell cinst AgentRansack --ignore-package-exit-codes
+powershell cinst sublimetext3 --ignore-package-exit-codes
+powershell cinst kdiff3 --ignore-package-exit-codes
