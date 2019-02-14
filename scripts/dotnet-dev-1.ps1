e9acf598a8e0ddbe22bb8e353be511e51c67a398
Colin Ng <colinn@orbitaltech.com>
2019-02-14 04:52:01 -0800

    Updated dev tools for building Windows apps.

--- scripts/dotnet-dev-1.ps1
+++ scripts/dotnet-dev-1.ps1
@@ -1,1 +1,1 @@
-powershell choco install VisualStudio2017Community --execution-timeout=7200 --ignore-package-exit-codes
+powershell cinst VisualStudio2017Community --execution-timeout=7200 --ignore-package-exit-codes
--- scripts/dotnet-dev-2.ps1
+++ scripts/dotnet-dev-2.ps1
@@ -5,6 +5,12 @@
 #Allowed exit codes are: [0]
 powershell cinst LINQPad --ignore-package-exit-codes
 
+# Visual Studio 2017 Workloads
+powershell cinst visualstudio2017-workload-manageddesktop
+powershell cinst visualstudio2017-workload-netcoretools
+#powershell cinst visualstudio2017-workload-netcrossplat
+#powershell cinst visualstudio2017-workload-netweb
+
 # General Usage
 powershell cinst GoogleChrome --ignore-package-exit-codes
 powershell cinst 7Zip --ignore-package-exit-codes
@@ -14,17 +20,17 @@ powershell cinst Firefox --forcex86 --ignore-package-exit-codes
 # geckodriver 0.18.0 has a bug with Maximize Window
 # https://github.com/mozilla/geckodriver/issues/820
 # that might be fixed with Firefox 55 which will be out 2017-08-08
-powershell cinst selenium-gecko-driver --version 0.17.0 --ignore-package-exit-codes
+#powershell cinst selenium-gecko-driver --version 0.17.0 --ignore-package-exit-codes
 powershell cinst nunit-console.portable --ignore-package-exit-codes
 
 # CI
-powershell cinst JRE8 --ignore-package-exit-codes
+#powershell cinst JRE8 --ignore-package-exit-codes
 
 # Developer/QA
 powershell cinst git --ignore-package-exit-codes
 powershell cinst SourceTree --ignore-package-exit-codes
 powershell cinst Putty --ignore-package-exit-codes
-powershell cinst sql-server-management-studio --ignore-package-exit-codes
+#powershell cinst sql-server-management-studio --ignore-package-exit-codes
 powershell cinst AgentRansack --ignore-package-exit-codes
 powershell cinst sublimetext3 --ignore-package-exit-codes
 powershell cinst kdiff3 --ignore-package-exit-codes
